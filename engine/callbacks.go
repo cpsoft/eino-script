4642581726ab147adb59b1bf4ab3f1f369306224
cposft <cpsoft@msn.com>
2025-06-19 14:48:21 +0800

    修改callback定义

--- engine/callbacks.go
+++
@@ -1,8 +0,0 @@
-package engine
-
-import "eino-script/types"
-
-type Callbacks interface {
-	GetModelInfo(modelName string) (*types.ModelInfo, error)
-	CreateMcpServer(mcpId string) (*types.IMcpServer, error)
-}
